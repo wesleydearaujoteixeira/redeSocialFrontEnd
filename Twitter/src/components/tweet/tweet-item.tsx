64ec4f45f1a94a60fff8aaf14e685ea6a8bc2129
Wesley <wesleyaraujoteixeira98@gmail.com>
2025-06-25 16:00:39 -0300

    mais ajustes

--- Twitter/src/components/tweet/tweet-item.tsx
+++ Twitter/src/components/tweet/tweet-item.tsx
@@ -139,7 +139,7 @@ export const TweetItem = () => {
                       />
                   </Link>
                 )}
-                <div className="flex items-center justify-center mt-4 text-gray-500 w-[40%] mx-auto">
+                <div className="flex items-center justify-center mt-4 text-gray-500 w-[56%] mx-auto">
                     <div className="flex-1">
                       <Link href={`/tweet/${post.id}`}>
                         <div className="inline-flex items-center gap-2 cursor-pointer">
